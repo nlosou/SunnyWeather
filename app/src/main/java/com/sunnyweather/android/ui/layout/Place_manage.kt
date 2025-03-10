4efbb9c17ff41144cbff504a936cd0cba4f166c6
nlosou <2128819713@qq.com>
2025-03-10 23:05:17 +0800

    Auto commit on 2025年 03月 10日 星期一 23:05:17 CST

--- app/src/main/java/com/sunnyweather/android/ui/layout/Place_manage.kt
+++ app/src/main/java/com/sunnyweather/android/ui/layout/Place_manage.kt
@@ -1,6 +1,7 @@
 package com.sunnyweather.android.ui.layout
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -17,15 +18,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sunnyweather.android.ui.component.SearchBar
 import com.sunnyweather.android.ui.layout.ui.theme.SunnyWeatherTheme
+import com.sunnyweather.android.ui.place.PlaceViewModel
+import com.sunnyweather.android.ui.weather.WeatherViewModel
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Place_manage(name: String, modifier: Modifier = Modifier) {
+fun Place_manage(PlaceViewModel:PlaceViewModel,WeatherViewModel:WeatherViewModel) {
     Scaffold(
         topBar = {
             IconButton(onClick = {
@@ -34,20 +39,6 @@ fun Place_manage(name: String, modifier: Modifier = Modifier) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "")
             }
         },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /* Do something */ }
-            ) {
-                Icon(Icons.Filled.Add, contentDescription = "Add")
-            }
-        },
-        bottomBar = {
-            BottomAppBar {
-                IconButton(onClick = { /* Do something */ }) {
-                    Icon(Icons.Filled.Menu, contentDescription = "Menu")
-                }
-            }
-        }
     ) { contentPadding ->
         // 主内容区域
         Box(
@@ -55,7 +46,11 @@ fun Place_manage(name: String, modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .padding(contentPadding)
         ) {
-            Text("Main content area", modifier = Modifier.align(Alignment.Center))
+            Column {
+                Text("Main content area")
+                SearchBar(PlaceViewModel,WeatherViewModel)
+            }
+
         }
     }
 }
@@ -64,6 +59,8 @@ fun Place_manage(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview10() {
     SunnyWeatherTheme {
-        Place_manage("Android")
+        var a= remember { PlaceViewModel() }
+        var b= remember { WeatherViewModel() }
+        Place_manage(a,b)
     }
 }
