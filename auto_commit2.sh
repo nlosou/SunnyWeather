eca44797c137afbaef8e441ffb62bbf01b608851
nlosou <2128819713@qq.com>
2025-03-15 21:33:30 +0800

    Auto commit on 2025年 03月 15日 星期六 21:33:30 CST

--- auto_commit2.sh
+++ auto_commit2.sh
@@ -2,11 +2,12 @@
 # 进入项目目录
 cd "$(dirname "$0")"
 
+
 # 定义新分支名称（需替换为你的目标分支名）
 NEW_BRANCH="ViewModel_Optimize"
 
 # 创建并切换到新分支（本地）
-git checkout -b $NEW_BRANCH
+#git checkout -b $NEW_BRANCH
 
 # 添加所有更改
 git add .
